64208750d8a1984d9f7cf99c2a31ffc98f2a3a0f
jayeshsupra <84458100+jayeshsupra@users.noreply.github.com>
2021-09-21 09:55:44 +0530

    renamed canister (#2)

--- dfinity_poc/src/dfinity_poc/lib.rs
+++ dfinity_poc/src/dfinity_poc/lib.rs
@@ -1,18 +1,15 @@
-mod verify_signature;
-
-extern crate secp256k1;
 extern crate hex;
+extern crate secp256k1;
 
 use ic_cdk_macros::*;
-use secp256k1::{Secp256k1, Message, Signature, PublicKey};
+use secp256k1::{Message, PublicKey, Secp256k1, Signature};
 use sha256::digest;
 
-
-static mut MESSAGE : String = String::new();
-static mut PUBLIC_KEY : String = String::new();
-static mut FLAG : bool = false;
+static mut MESSAGE: String = String::new();
+static mut PUBLIC_KEY: String = String::new();
+static mut FLAG: bool = false;
 #[update]
-fn store_message(pub_key : String, sign : String, msg : String) -> String {
+fn store_message(pub_key: String, sign: String, msg: String) -> String {
     ic_cdk::print(format!("Public Key ==> {:?}", pub_key));
     ic_cdk::print(format!("Signature ==> {:?}", sign));
     ic_cdk::print(format!("Message ==> {:?}", msg));
@@ -22,106 +19,108 @@ fn store_message(pub_key : String, sign : String, msg : String) -> String {
         ic_cdk::print(format!("Message ==> {:?}", msg_hash));
 
         let secp = Secp256k1::verification_only();
-        let public_key = PublicKey::from_slice(&hex::decode(&pub_key).unwrap()).expect("public keys must be 33 or 65 bytes, serialized according to SEC 2");
-        let message = Message::from_slice(&hex::decode(&msg_hash).unwrap()).expect("messages must be 32 bytes and are expected to be hashes");
+        let public_key = PublicKey::from_slice(&hex::decode(&pub_key).unwrap())
+            .expect("public keys must be 33 or 65 bytes, serialized according to SEC 2");
+        let message = Message::from_slice(&hex::decode(&msg_hash).unwrap())
+            .expect("messages must be 32 bytes and are expected to be hashes");
         if PUBLIC_KEY.eq(&pub_key) {
             ic_cdk::print(format!("Public Key Authenticated "));
             FLAG = true;
-
-        }else{
+        } else {
             ic_cdk::print(format!("Public Key Not Authenticated "));
             FLAG = false;
         }
 
-
         if FLAG {
-            let sig =Signature::from_compact(&hex::decode(&sign).unwrap()).expect("compact signatures are 64 bytes; DER signatures are 68-72 bytes");
+            let sig = Signature::from_compact(&hex::decode(&sign).unwrap())
+                .expect("compact signatures are 64 bytes; DER signatures are 68-72 bytes");
 
             ic_cdk::print(format!("Signature => {:?}", &sig));
-            ic_cdk::print(format!("Verify => {:?}", &secp.verify(&message, &sig, &public_key).is_ok()));
-            let is_verify : bool = secp.verify(&message, &sig, &public_key).is_ok();
+            ic_cdk::print(format!(
+                "Verify => {:?}",
+                &secp.verify(&message, &sig, &public_key).is_ok()
+            ));
+            let is_verify: bool = secp.verify(&message, &sig, &public_key).is_ok();
 
-            if is_verify{
+            if is_verify {
                 update_message(msg);
                 "Signature Verified ,Message Will be Store".to_string()
-            }else{
+            } else {
                 "Signature not verified , Message not store".to_string()
             }
-        }else{
+        } else {
             "Public key not verified , Message not store".to_string()
         }
     }
-
 }
 
 #[update]
-fn store_pub_key(pub_key : String, sign : String, msg : String) -> String {
+fn store_pub_key(pub_key: String, sign: String, msg: String) -> String {
     ic_cdk::print("UPDATING NEW PUBLIC KEY");
     ic_cdk::print(format!("Public Key ==> {:?}", pub_key));
     ic_cdk::print(format!("Signature ==> {:?}", sign));
     ic_cdk::print(format!("Message ==> {:?}", msg));
 
     unsafe {
-        if FLAG{
+        if FLAG {
             let msg_hash = digest(&msg);
             ic_cdk::print(format!("Message ==> {:?}", msg_hash));
             let secp = Secp256k1::verification_only();
-            let public_key = PublicKey::from_slice(&hex::decode(&pub_key).unwrap()).expect("public keys must be 33 or 65 bytes, serialized according to SEC 2");
-            let message = Message::from_slice(&hex::decode(&msg_hash).unwrap()).expect("messages must be 32 bytes and are expected to be hashes");
+            let public_key = PublicKey::from_slice(&hex::decode(&pub_key).unwrap())
+                .expect("public keys must be 33 or 65 bytes, serialized according to SEC 2");
+            let message = Message::from_slice(&hex::decode(&msg_hash).unwrap())
+                .expect("messages must be 32 bytes and are expected to be hashes");
             if PUBLIC_KEY.eq(&pub_key) {
                 ic_cdk::print(format!("Public Key Authenticated "));
                 FLAG = true;
-
-            }else{
+            } else {
                 ic_cdk::print(format!("Public Key Not Authenticated "));
                 FLAG = false;
             }
             if FLAG {
-                let sig =Signature::from_compact(&hex::decode(&sign).unwrap()).expect("compact signatures are 64 bytes; DER signatures are 68-72 bytes");
+                let sig = Signature::from_compact(&hex::decode(&sign).unwrap())
+                    .expect("compact signatures are 64 bytes; DER signatures are 68-72 bytes");
                 ic_cdk::print(format!("Signature => {:?}", &sig));
-                ic_cdk::print(format!("Verify => {:?}", &secp.verify(&message, &sig, &public_key).is_ok()));
-                let is_verify : bool = secp.verify(&message, &sig, &public_key).is_ok();
-                if is_verify{
-
+                ic_cdk::print(format!(
+                    "Verify => {:?}",
+                    &secp.verify(&message, &sig, &public_key).is_ok()
+                ));
+                let is_verify: bool = secp.verify(&message, &sig, &public_key).is_ok();
+                if is_verify {
                     // let msg1 = String::from("04").push_str(&msg);
-                    let msg1 = format!("04{msg}", msg=msg.clone());
+                    let msg1 = format!("04{msg}", msg = msg.clone());
                     PUBLIC_KEY.clear();
-                    PUBLIC_KEY.push_str(msg1.as_str()) ;
+                    PUBLIC_KEY.push_str(msg1.as_str());
                     "New Public key inserted".to_string()
-                }else{
+                } else {
                     "Signature not verified , Message not store".to_string()
                 }
-            }else{
+            } else {
                 "Public key not verified , Message not store".to_string()
             }
-        }else{
+        } else {
             "No Public key available".to_string()
         }
     }
 }
 
-
-
-
 #[update]
-fn update_message(message : String)
-{
+fn update_message(message: String) {
     unsafe {
         MESSAGE.clear();
-        MESSAGE.push_str(message.as_str()) ;
+        MESSAGE.push_str(message.as_str());
     }
 }
 
 #[update]
-fn update_key(key : String){
+fn update_key(key: String) {
     unsafe {
-        if FLAG{
+        if FLAG {
             "Public Key is already there".to_string();
-        }else{
+        } else {
             PUBLIC_KEY.clear();
-            PUBLIC_KEY.push_str(key.as_str()) ;
+            PUBLIC_KEY.push_str(key.as_str());
         }
-
     }
 }
 
